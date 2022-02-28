2b4ade1d3b5ddc64ba21ecaaeef6fb61f008f1fb
Frank Pagan <frank@cocreate.app>
2022-02-28 06:24:15 +0000

    fix: config docs wildcard * domain

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 "name": "index.html",
                 "path": "/docs/ide/index.html",
                 "domains": [
-                    "cocreate.app",
+                    "*",
                     "general.cocreate.app"
                 ],
                 "directory": "/docs/ide",
